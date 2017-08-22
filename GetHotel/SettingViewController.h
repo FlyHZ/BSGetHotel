66d0737e8ae5b5636149a93f67214c668e0e3c32
Lubin <1589507430@qq.com>
2017-08-22 09:26:16 +0800

    我的

---
+++ GetHotel/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  GetHotels
+//
+//  Created by admin1 on 2017/8/22.
+//  Copyright © 2017年 Yixin studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
